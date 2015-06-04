c8f0457bb9ccec257dbc35f67ff3d399d20cf67c
Nico769 <alucard769@gmail.com>
2015-06-04 23:43:04 +0200

    Indentazione migliorata

--- provaConnessioneDB/src/com/example/provaconnessionedb/MainActivity.java
+++ provaConnessioneDB/src/com/example/provaconnessionedb/MainActivity.java
@@ -10,7 +10,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
-import android.app.Activity;
+//import android.app.Activity;
 
 
 
--- provaConnessioneDB/src/com/team10/Comms/User.java
+++ provaConnessioneDB/src/com/team10/Comms/User.java
@@ -7,8 +7,17 @@ import java.util.Calendar;
 
 public class User {
 	
+	
+//  2.3.40:	UC-Read Message
+/* PRECONDITONS:
+* Il programma deve conoscere la locazione del DataBase; Deve essere gia
+* inizializzata la struttura PROCESS:User.
+* 
+* POSTCONDITIONS:
+* Il sistema deve consentire ad un utente la lettura di un messaggio.
+*/
+	
 	public void read_message(int id){
-		
 	
 		Connection conn=null;
 		final String user = "b76a43fe274f2a";
@@ -52,77 +61,52 @@ public class User {
 * 
 * POSTCONDITIONS:
 * Il sistema deve consentire ad un utente di inviare messaggi di testo ad un qualsiasi altro utente.
-   */
-public void Send_Message(String mail, String object, String text)
-{
+*/
+   public void Send_Message(String mail, String object, String text) {
 
-    //Connessione al db e lancio di eccezione in caso di fallimento.
-	
+    //Connessione al db e lancio di eccezione in caso di fallimento.	
 	Connection conn=null;
 	final String user = "b76a43fe274f2a";
 	final String psw = "b1a0a642";
 	
 	try {
-	conn = DatabaseConnect.estabilishConnection(conn,user,psw);
+		conn = DatabaseConnect.estabilishConnection(conn,user,psw);
 	
-    //Crea una SQL date object in modo da usarla nell'INSERT statement
-	Calendar calendar = Calendar.getInstance();
-	java.sql.Date sendDate = new java.sql.Date(calendar.getTime().getTime());
+		//Crea una SQL date object in modo da usarla nell'INSERT statement
+		Calendar calendar = Calendar.getInstance();
+		java.sql.Date sendDate = new java.sql.Date(calendar.getTime().getTime());
 	
-	//cerca l'utente nel db; se non e' presente, lancia l'errore.
-	//TODO Risolvere la +mail+
-	Statement st = conn.createStatement();
-	ResultSet rs = st.executeQuery("SELECT * FROM users WHERE Mail = '"+mail+"' ");
+		//cerca l'utente nel db; se non e' presente, lancia l'errore.
+		//TODO Risolvere la +mail+
+		Statement st = conn.createStatement();
+		ResultSet rs = st.executeQuery("SELECT * FROM users WHERE Mail = '"+mail+"' ");
 	
-	//Se l'utente e' presente, immette una nuova tupla in DATABASE:MESSAGES; quindi invia un messaggio all'utente desiderato.
+		//Se l'utente e' presente, immette una nuova tupla in DATABASE:MESSAGES; quindi invia un messaggio all'utente desiderato.
 	
-	String query = " insert into messages (Sender, Receiver, SendingDate, isRead, Object, Text)"
-	+ " values (?, ?, ?, ?, ?, ?)";
+		String query = " insert into messages (Sender, Receiver, SendingDate, isRead, Object, Text)"
+		+ " values (?, ?, ?, ?, ?, ?)";
 
-	// Creo il mySQL insert PreparedStatement
-	PreparedStatement preparedStmt = conn.prepareStatement(query);
-	preparedStmt.setString (2, "email@email.com");
-	preparedStmt.setString (3, mail);
-	preparedStmt.setDate   (4, sendDate);
-	preparedStmt.setBoolean (5, false);
-	preparedStmt.setString (6, object);
-	preparedStmt.setString (7, text);
+		// Creo il mySQL insert PreparedStatement
+		PreparedStatement preparedStmt = conn.prepareStatement(query);
+		preparedStmt.setString (2, "email@email.com");
+		preparedStmt.setString (3, mail);
+		preparedStmt.setDate   (4, sendDate);
+		preparedStmt.setBoolean (5, false);
+		preparedStmt.setString (6, object);
+		preparedStmt.setString (7, text);
 
-	// Esecuzione del PreparedStatement
-	preparedStmt.execute();
+		// Esecuzione del PreparedStatement
+		preparedStmt.execute();
 
-	// Esegue la disconnessione dal DataBase
-	DatabaseConnect.closeConnection(conn);
-	// Elimino dalla memoria gli oggetti usati 
-    rs=null;
-    st=null;
-    }catch (SQLException ex) {
-        ex.printStackTrace();
-	  }
-}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+		// Esegue la disconnessione dal DataBase
+		DatabaseConnect.closeConnection(conn);
+		// Elimino dalla memoria gli oggetti usati 
+    	rs=null;
+    	st=null;
+    }   catch (SQLException ex) {
+          ex.printStackTrace();
+	    }
+   }
 	
 	
 }
